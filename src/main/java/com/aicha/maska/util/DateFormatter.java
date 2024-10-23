54cdc7074ffd4cedf545d727a03a5dcc2fcc3415
ettabet aicha <aicha70tabite@gmail.com>
2024-10-23 22:49:16 +0100

    add util for fix problem formatdate

---
+++ src/main/java/com/aicha/maska/util/DateFormatter.java
@@ -0,0 +1,27 @@
+package com.aicha.maska.util;
+
+import org.springframework.format.Formatter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateFormatter implements Formatter<Date> {
+    private String datePattern;
+    private SimpleDateFormat dateFormat;
+
+    public DateFormatter(String datePattern) {
+        this.datePattern = datePattern;
+        this.dateFormat = new SimpleDateFormat(datePattern);
+    }
+
+    @Override
+    public Date parse(String text, Locale locale) throws ParseException {
+        return dateFormat.parse(text);
+    }
+
+    @Override
+    public String print(Date object, Locale locale) {
+        return dateFormat.format(object);
+    }
+}
